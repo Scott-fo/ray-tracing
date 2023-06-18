9677a4b79a3dce19a05ced9e254746bae0d556b6
Scott-fo <scottfo@live.com>
2023-06-18 19:00:47 +0100

    feat: ray traced sphere

---
+++ color.h
@@ -0,0 +1,14 @@
+#ifndef COLOR_H
+#define COLOR_H
+
+#include "vec3.h"
+#include <iostream>
+#include <ostream>
+
+inline void write_colour(std::ostream &out, colour pixel_colour) {
+  out << static_cast<int>(255.999 * pixel_colour.x()) << ' '
+      << static_cast<int>(255.999 * pixel_colour.y()) << ' '
+      << static_cast<int>(255.999 * pixel_colour.z()) << '\n';
+}
+
+#endif
