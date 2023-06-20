3edad4fd1f99d9ab13803caf7e853d39acec9ecd
Scott-fo <scottfo@live.com>
2023-06-20 20:23:30 +0100

    feat: anti aliasing

--- color.h
+++ color.h
@@ -1,14 +1,25 @@
 #ifndef COLOR_H
 #define COLOR_H
 
+#include "utils.h"
 #include "vec3.h"
 #include <iostream>
 #include <ostream>
 
-inline void write_colour(std::ostream &out, colour pixel_colour) {
-  out << static_cast<int>(255.999 * pixel_colour.x()) << ' '
-      << static_cast<int>(255.999 * pixel_colour.y()) << ' '
-      << static_cast<int>(255.999 * pixel_colour.z()) << '\n';
+inline void write_colour(std::ostream &out, colour pixel_colour,
+                         int samples_per_pixel) {
+  auto r = pixel_colour.x();
+  auto g = pixel_colour.y();
+  auto b = pixel_colour.z();
+
+  auto scale = 1.0 / samples_per_pixel;
+  r *= scale;
+  g *= scale;
+  b *= scale;
+
+  out << static_cast<int>(256 * clamp(r, 0.0, 0.999)) << ' '
+      << static_cast<int>(256 * clamp(g, 0.0, 0.999)) << ' '
+      << static_cast<int>(256 * clamp(b, 0.0, 0.999)) << '\n';
 }
 
 #endif
