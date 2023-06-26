9c49be9639e7e5d0e13d5a2d7c6494c0e443bdc5
Scott-fo <scottfo@live.com>
2023-06-26 18:13:18 +0100

    schlick approximation

--- include/material.h
+++ include/material.h
@@ -3,6 +3,7 @@
 
 #include "hittable.h"
 #include "ray.h"
+#include "utils.h"
 #include "vec3.h"
 
 struct hit_record;
@@ -49,6 +50,47 @@ public:
 public:
   colour albedo;
   double fuzz;
+};
+
+class dielectric : public material {
+public:
+  dielectric(double index_of_refraction) : ir(index_of_refraction) {}
+
+  virtual bool scatter(const ray &r_in, const hit_record &rec,
+                       colour &attenuation, ray &scattered) const override {
+    attenuation = colour(1.0, 1.0, 1.0);
+    double refraction_ratio = rec.front_face ? (1.0 / ir) : ir;
+
+    vec3 unit_direction = unit_vector(r_in.direction());
+
+    double cos_theta = fmin(dot(-unit_direction, rec.normal), 1.0);
+    double sin_theta = sqrt(1.0 - cos_theta * cos_theta);
+
+    bool total_internal_reflection = refraction_ratio * sin_theta > 1.0;
+
+    vec3 direction;
+    if (total_internal_reflection ||
+        reflectance(cos_theta, refraction_ratio) > random_double()) {
+      direction = reflect(unit_direction, rec.normal);
+    } else {
+      direction = refract(unit_direction, rec.normal, refraction_ratio);
+    }
+
+    scattered = ray(rec.p, direction);
+
+    return true;
+  }
+
+public:
+  double ir;
+
+private:
+  static double reflectance(double cosine, double ref_idx) {
+    auto r0 = (1 - ref_idx) / (1 + ref_idx);
+    r0 *= r0;
+
+    return r0 + (1 - r0) * pow((1 - cosine), 5);
+  }
 };
 
 #endif
